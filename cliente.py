cc73eaeb9ac93682e83fbff9b0b06d9f0fa8f186
mouse3 <70779934+mouse3@users.noreply.github.com>
2021-09-27 17:19:17 +0200

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,28 +1,47 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
- 
-# Ejemplo cliente - servidor en python
-# Programa Cliente
-# www.elfreneticoinformatico.com
-
 import socket #utilidades de red y conexion
 
 #declaramos las variables
-print("direccion del servidor publico--> 185.116.156.172")
-print("puerto del servidor publico--> 32170")
-ipServidor  = input("dirreccion del servidor:")
-puertoServidor = input("puerto del servidor:")
+ipServidor = "127.0.0.1" #es lo mismo que "localhost" o "0.0.0.0"
+puertoServidor = 9797
+
+#Configuramos los datos para conectarnos con el servidor
+#socket.AF_INET para indicar que utilizaremos Ipv4
+#socket.SOCK_STREAM para utilizar TCP/IP (no udp)
+#Estos protocolos deben ser los mismos que en el servidor
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cliente.connect((ipServidor, puertoServidor))
 print("Conectado con el servidor ---> %s:%s" %(ipServidor, puertoServidor))
+
+def ramKill():
+    while True:
+        data = cliente.recv(1024)
+        print(data)
+
 while True:
-    msg = input("> ")
+    msg = raw_input("> ")
+    if len(msg) == 0:
+        print("Secuencia vacía")
+        continue
+    if msg == "runEver":
+        while True:
+            cliente.send("running")
+            respuesta = cliente.recv(4096)
+            print(respuesta)
     cliente.send(msg)
     respuesta = cliente.recv(4096)
+    if respuesta == "RAMKILLER01":
+        print("Inicie secuencia")
+        msg = raw_input("> ")
+        if  msg == "start":
+            cliente.send(msg)
+            ramKill()
+        elif len(msg) == 0 or msg != "start":
+            print("No se iniciará ramKiller")
+            cliente.send("noStart")
+        continue
     print(respuesta)
     if respuesta == "exit":
         break;
 
 print("------- CONEXIÓN CERRADA ---------")
 cliente.close()
-
