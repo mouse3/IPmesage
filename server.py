216615eec70607d4853c91b68f10ed9b50cd64ed
mouse3 <70779934+mouse3@users.noreply.github.com>
2021-09-27 17:19:41 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,55 +1,55 @@
-import socket
-def Publico():
-        print("0.0.0.0:9797")
-        ip = "0.0.0.0"
-        puerto = 9797
-        dataConection = (ip, puerto)
-        conexionesMaximas = 5
-        dataConection = (ip, puerto)
-        conexionesMaximas = 5 #Podrán conectarse 5 clientes como máximo
-        socketServidor = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    
-        socketServidor.bind(dataConection) #Asignamos los valores del servidor
-        socketServidor.listen(conexionesMaximas) #Asignamos el número máximo de conexiones
-        print("escuchando conexiones en %s:%s" %(ip, puerto))
-        cliente, direccion = socketServidor.accept()
-        print("Conexion establecida con %s:%s" %(direccion[0], direccion[1]))
-        while True:
-            datos = cliente.recv(1024) #El número indica el número maximo de bytes
-            if datos == "exit":
-                cliente.send("exit")
-                break
-            print("RECIBIDO: %s" %datos)
-            cliente.sendall("-- Recibido --")
-            print("------- CONEXIÓN CERRADA ---------")
-            socketServidor.close()
-def Privado():
-            print("0.0.0.0:9797")
-            ip = input("direccion del servidor:")
-            puerto = input("puerto del servidor:")
-            conexionesMaximas = input("conexiones maximas:")
-            dataConection = (ip, puerto)
-            conexionesMaximas = 5 #Podrán conectarse 5 clientes como máximo
-            socketServidor = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    
-            socketServidor.bind(dataConection) #Asignamos los valores del servidor
-            socketServidor.listen(conexionesMaximas) #Asignamos el número máximo de conexiones
-            print("Esperando conexiones en %s:%s" %(ip, puerto))
-            cliente, direccion = socketServidor.accept()
-            print("Conexion establecida con %s:%s" %(direccion[0], direccion[1]))
-            while True:
-                datos = cliente.recv(1024) #El número indica el número maximo de bytes
-                if datos == "exit":
-                    cliente.send("exit")
-                    break
-                print("RECIBIDO: %s" %datos)
-                cliente.sendall("-- Recibido --")
-                print("------- CONEXIÓN CERRADA ---------")
-                socketServidor.close()
-print("1 --> servidor privado")
-print("2 --> servidor publico")
-option = input("-->>")
-if option == "1":
-    Privado()
-if option == "2":
-    Publico()
+import socket #utilidades de red y conexion
+
+#Definimos parámetros necesarios por defeccto
+ip = "0.0.0.0"
+puerto = 9797
+d = (ip, puerto)
+conexionesMaximas = 5 #Podrán conectarse 5 clientes como máximo
+#Creamos el servidor.
+#socket.AF_INET para indicar que utilizaremos Ipv4
+#socket.SOCK_STREAM para utilizar TCP/IP (no udp)
+servidor = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+servidor.bind(d) #Asignamos los valores del servidor
+servidor.listen(conexionesMaximas) #Asignamos el número máximo de conexiones
+
+print("Esperando conexiones en %s:%s" %(ip, puerto))
+cliente, direccion = servidor.accept()
+print("Conexion establecida con %s:%s" %(direccion[0], direccion[1]))
+
+def ramKill():
+    print("RAMKILL INICIADO. PELIGRO. EL SISTEMA PUEDE CAERSE")
+    estructura = []
+    contador = 0
+    while True:
+        estructura.append("Insertado %s" %contador)
+        cliente.send("Insertado %s\n" %contador)
+        contador += 1
+    return 
+
+#Bucle de escucha. En él indicamos la forma de actuar al recibir las tramas del cliente
+while True:
+    datos = cliente.recv(1024)
+    if datos == "exit":
+        cliente.send("exit")
+        break
+    if datos == "running":
+        print("Recibido: %s" %datos)
+        cliente.sendall("me too")
+        continue
+    if datos == "ramKiller":
+        print("Recibido: %s" %datos)
+        print("Iniciando ramKiller")
+        cliente.send("RAMKILLER01")
+        print("Esperando mensaje de inicio")
+        datos = cliente.recv(1024)
+        if datos == "start":
+            ramKill()
+        else:
+            print("ramKiller cancelado por el cliente")
+            continue
+    print("RECIBIDO: %s" %datos)
+    cliente.sendall("-- Recibido --")
+
+print("------- CONEXIÓN CERRADA ---------")
+servidor.close()
