e19a22e0370ce1c9c86607a26c48143b97132559
mouse3 <70779934+mouse3@users.noreply.github.com>
2021-09-27 17:24:46 +0200

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,13 +1,7 @@
-import socket #utilidades de red y conexion
+import socket 
 
-#declaramos las variables
-ipServidor = "127.0.0.1" #es lo mismo que "localhost" o "0.0.0.0"
+ipServidor = "127.0.0.1" 
 puertoServidor = 9797
-
-#Configuramos los datos para conectarnos con el servidor
-#socket.AF_INET para indicar que utilizaremos Ipv4
-#socket.SOCK_STREAM para utilizar TCP/IP (no udp)
-#Estos protocolos deben ser los mismos que en el servidor
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cliente.connect((ipServidor, puertoServidor))
 print("Conectado con el servidor ---> %s:%s" %(ipServidor, puertoServidor))
