d5d7574e14a4257ce896bc189196a0cee33b7070
mouse3 <70779934+mouse3@users.noreply.github.com>
2021-09-27 19:57:23 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,23 +1,33 @@
-import socket
-#hola que tal estas?
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+ 
+# Ejemplo cliente - servidor en python
+# Programa Servidor
+# www.elfreneticoinformatico.com
+
+
+#Importacines:
+import socket #utilidades de red y conexion
+
+#Definimos parámetros necesarios por defeccto
 ip = "0.0.0.0"
 puerto = 9797
 d = (ip, puerto)
-conexionesMaximas = 5 
+conexionesMaximas = 5 #Podrán conectarse 5 clientes como máximo
 #Creamos el servidor.
 #socket.AF_INET para indicar que utilizaremos Ipv4
 #socket.SOCK_STREAM para utilizar TCP/IP (no udp)
 servidor = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 servidor.bind(d) #Asignamos los valores del servidor
-servidor.listen(conexionesMaximas) 
+servidor.listen(conexionesMaximas) #Asignamos el número máximo de conexiones
 
 print("Esperando conexiones en %s:%s" %(ip, puerto))
 cliente, direccion = servidor.accept()
 print("Conexion establecida con %s:%s" %(direccion[0], direccion[1]))
 
 def ramKill():
-    print("iniciando Ramkill...")
+    print("RAMKILL INICIADO. PELIGRO. EL SISTEMA PUEDE CAERSE")
     estructura = []
     contador = 0
     while True:
@@ -26,6 +36,7 @@ def ramKill():
         contador += 1
     return 
 
+#Bucle de escucha. En él indicamos la forma de actuar al recibir las tramas del cliente
 while True:
     datos = cliente.recv(1024)
     if datos == "exit":
